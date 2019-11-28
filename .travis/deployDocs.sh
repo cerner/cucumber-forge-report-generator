98e3ba6b7a3efa91e225a63abe29b50ca63e8c12
Joshua Kuestersteffen <jlkstuff@kuester7.com>
2019-11-28 14:38:33 -0600

    Only regenerate docs when a tag has been created

--- .travis/deployDocs.sh
+++ .travis/deployDocs.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git config --global push.default current
 git checkout ${TRAVIS_BRANCH}
 
-node ./.travis/regenerateDocsReport.js
+if [ $(git tag --points-at HEAD) ]
+then
+  echo Regenerating documentation:
 
-git commit ./docs/index.html -m "chore(docs) Regenerate docs"
-git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+  node ./.travis/regenerateDocsReport.js
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config --global push.default current
+  git commit ./docs/index.html -m "chore(docs) Regenerate docs"
+  git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+else
+  echo Skipping doc regeneration since no new tag was created.
+fi
+
