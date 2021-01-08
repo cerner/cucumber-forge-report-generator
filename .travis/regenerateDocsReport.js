da65e3f07698161bc251d9edc2dd0dfb902295eb
Tobiah <tobiah@pm.me>
2021-01-07 22:38:58 -0600

    chore(ci): migrate to gh actions

--- .travis/regenerateDocsReport.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const Generator = require('../src/Generator');
-
-const FILE_ENCODING = 'utf-8';
-
-const report = new Generator().generate(path.resolve(__dirname, '../'), 'cucumber-forge-report-generator')
-fs.writeFileSync(path.resolve(__dirname, '../docs/index.html'), report, FILE_ENCODING);
