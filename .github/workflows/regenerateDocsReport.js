e52e5019cca80ee80ed7a4b48e4630709e9a438c
Matt Voboril <matej@voboril.org>
2021-04-07 17:41:50 -0500

    fix(ci): correct script to check for docs folder

--- .github/workflows/regenerateDocsReport.js
+++ .github/workflows/regenerateDocsReport.js
@@ -5,5 +5,13 @@ const Generator = require('../../src/Generator');
 
 const FILE_ENCODING = 'utf-8';
 
-const report = new Generator().generate(path.resolve(__dirname, '../../'), 'cucumber-forge-report-generator')
-fs.writeFileSync(path.resolve(__dirname, '../../docs/index.html'), report, FILE_ENCODING);
+const featuresDir = path.resolve(__dirname, '../../');
+const report = new Generator().generate(featuresDir, 'cucumber-forge-report-generator')
+
+// Create /docs if it doesn't exist
+const outputDir = path.resolve(__dirname, '../../docs');
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
+}
+const filePath = path.resolve(outputDir, 'index.html');
+fs.writeFileSync(filePath, report, FILE_ENCODING);
